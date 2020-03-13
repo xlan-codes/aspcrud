d35b95657940f5c08ab036b9267b1c6c03854b62
Hekuran Islamaj <hekuran.islamai@gmail.com>
2020-03-13 13:44:55 +0100

    init project

---
+++ CRUDOperations/Controllers/UsersController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUDOperations.Models;
+
+namespace CRUDOperations.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Index(string searchString)
+        {
+            if (searchString == null) {
+                searchString = "";
+            }
+            using (var user = new UsersEntities())
+            {
+                var users = user.People.ToList().Where(x => x.FirstName.Contains(searchString) || x.LastName.Contains(searchString)).Select(x => new UsersViewModel
+                {
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Birthdate = x.Birthdate,
+                    Birthplace = x.Birthplace,
+                    MartialStatus = x.MartialStatus,
+                    Gender = x.Gender,
+                    Employed = x.Employed,
+                    Phone = x.Phone,
+                    UserId = x.UserId
+                });
+                return View(users);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Search(string searchString)
+        {
+            using (var user = new UsersEntities())
+            {
+                var users = user.People.ToList().Where(x => x.FirstName.Contains(searchString)).Where(x => x.FirstName.Contains(searchString)).Select(x => new UsersViewModel
+                {
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Birthdate = x.Birthdate,
+                    Birthplace = x.Birthplace,
+                    MartialStatus = x.MartialStatus,
+                    Gender = x.Gender,
+                    Employed = x.Employed,
+                    Phone = x.Phone,
+                    UserId = x.UserId
+                });
+                return View(users);
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(UsersViewModel objuser)
+        {
+            var User = new Person {Birthdate=objuser.Birthdate, MartialStatus=objuser.MartialStatus, Phone=objuser.Phone, Birthplace=objuser.Birthplace,Employed=objuser.Employed, Gender=objuser.Gender ,FirstName = objuser.FirstName, LastName = objuser.LastName };
+            using (var user = new UsersEntities())
+            {
+                user.People.Add(User);
+                user.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(Int64? id)
+        {
+            var userentities = new UsersEntities();
+            var user = userentities.People.Find(id);
+            UsersViewModel objuser = new UsersViewModel();
+            objuser.Birthdate = user.Birthdate;
+            objuser.MartialStatus = user.MartialStatus;
+            objuser.Phone = user.Phone;
+            objuser.Birthplace = user.Birthplace;
+            objuser.Employed = user.Employed;
+            objuser.Gender = user.Gender;
+            objuser.FirstName = user.FirstName;
+            objuser.LastName = user.LastName;
+            objuser.UserId = user.UserId;
+            return View(objuser);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(UsersViewModel user)
+        {
+            using (var users = new UsersEntities())
+            {
+                var objuser = users.People.Find(user.UserId);
+                objuser.Birthdate = user.Birthdate;
+                objuser.MartialStatus = user.MartialStatus;
+                objuser.Phone = user.Phone;
+                objuser.Birthplace = user.Birthplace;
+                objuser.Employed = user.Employed;
+                objuser.Gender = user.Gender;
+                objuser.FirstName = user.FirstName;
+                objuser.LastName = user.LastName;
+                users.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(Int64? id)
+        {
+            UsersViewModel objuser = new UsersViewModel();
+            using (var user = new UsersEntities())
+            {
+                var oneuser = user.People.Find(id);
+                objuser.Birthdate = oneuser.Birthdate;
+                objuser.MartialStatus = oneuser.MartialStatus;
+                objuser.Phone = oneuser.Phone;
+                objuser.Birthplace = oneuser.Birthplace;
+                objuser.Employed = oneuser.Employed;
+                objuser.Gender = oneuser.Gender;
+                objuser.FirstName = oneuser.FirstName;
+                objuser.LastName = oneuser.LastName;
+            }
+            return View(objuser);
+        }
+
+        public ActionResult Delete(Int64? id)
+        {
+            UsersViewModel objuser = new UsersViewModel();
+            using (var user = new UsersEntities())
+            {
+                var oneuser = user.People.Find(id);
+                objuser.Birthdate = oneuser.Birthdate;
+                objuser.MartialStatus = oneuser.MartialStatus;
+                objuser.Phone = oneuser.Phone;
+                objuser.Birthplace = oneuser.Birthplace;
+                objuser.Employed = oneuser.Employed;
+                objuser.Gender = oneuser.Gender;
+                objuser.FirstName = oneuser.FirstName;
+                objuser.LastName = oneuser.LastName;
+                objuser.UserId = oneuser.UserId;
+            }
+            return View(objuser);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(UsersViewModel objuser)
+        {
+            using (var user = new UsersEntities())
+            {
+                var oneuser = user.People.Find(objuser.UserId);
+                user.People.Attach(oneuser);
+                user.Entry(oneuser).State = System.Data.EntityState.Deleted;
+                user.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUDOperations/Models/UsersViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDOperations.Models
+{
+    public class UsersViewModel
+    {
+        public Int64 UserId { get; set; }
+        [Required(ErrorMessage="Please Enter First Name")]
+        [Display(Name="First Name")]
+        public string FirstName { get; set; }        
+        
+        
+        [Required(ErrorMessage="Please Enter Last Name")]
+        [Display(Name="Last Name")]
+        public string LastName { get; set; }        
+        
+
+        [Required(ErrorMessage="Please Enter Birthplace")]
+        [Display(Name="Birthplace")]
+        public string Birthplace { get; set; }        
+
+        [Required(ErrorMessage= "Please Enter Birthdate")]
+        [Display(Name= "Birthdate")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public Nullable<DateTime> Birthdate { get; set; }        
+        
+        [Required(ErrorMessage= "Please Enter Phone")]
+        [Display(Name= "Phone")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Gender")]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Employed")]
+        [Display(Name = "Employed")]
+        public bool Employed { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Martial Status")]
+        [Display(Name = "Martial Status")]
+        public string MartialStatus { get; set; }
+
+
+       public string toString(string format)
+        {
+            return ((DateTime)this.Birthdate).ToString(format);
+        }
+
+    }
+}
